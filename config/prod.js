93f7d4bda519f31c1da4f1b13a1a525ba9194edc
Nagendra Sharma <nagendra.sharma@cybersoft.net>
2020-06-03 11:22:22 +0530

    Update prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,5 +1,5 @@
 module.exports = {
-  DB_URI: process.env.DB_URI,
-  SESSION_SECRET: process.env.SESSION_SECRET,
-  JWT_SECRET: process.env.JWT_SECRET
+  DB_URI: 'mongodb+srv://testuser:testUser@meetupcluster-vuwbv.mongodb.net/test?retryWrites=true&w=majority',
+  SESSION_SECRET: 'ddsrerrrresddredfr',
+  JWT_SECRET: 'ddsrerrrresddredfr'
 }
